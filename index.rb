89b4a5dfa853b4948aca0dd31bdb2f886b9b29e2
garybunofsky <garybunofsky@gmail.com>
2016-07-14 20:49:12 -0400

    Veronica can now small talk among other things.

--- index.rb
+++ index.rb
@@ -2,17 +2,36 @@ require 'sinatra/base'
 require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
-
+require 'api-ai-ruby'
 class Veronica < Sinatra::Base
 
 	post '/' do
-	  twiml = Twilio::TwiML::Response.new do |r|
-	    r.Message params[:Body]
+
+		# Establish connection with api.ai
+		client = ApiAiRuby::Client.new(
+				:client_access_token => 'e1d0afd773944e738d57d44658e543aa',
+		)
+
+		# If inbound message is sent to Veronica
+		message = params[:Body]
+		if message
+			response = client.text_request message
+			twiml = Twilio::TwiML::Response.new do |r|
+				# See if Veronica understands the message
+				reply = response[:result][:speech]
+				if reply.empty?
+					r.Message "Sorry, I don't understand."
+				else
+					r.Message reply
+				end
+			end
+			twiml.text
 		end
-	  twiml.text
+
 	end
 
 	get '/' do
-		'Hi my name is veronica.'
+		'Hi, my name is Veronica'
 	end
+
 end
